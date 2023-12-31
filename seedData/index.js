c1ae8bcb75770f313d0bac89a1c177a94f6b3c7a
MingJu Sun <20104599@mail.wit.ie>
2023-12-31 07:28:35 +0000

    Changes due to API from WAD2 lab

--- seedData/index.js
+++ seedData/index.js
@@ -1,13 +1,13 @@
 import userModel from '../api/users/userModel';
-import users from './users';
-import genreModel from '../api/genres/genreModel';
-import genres from './genres';
-import movieModel from '../api/movies/movieModel';
-import movies from './movies.js';
+import users from '../initialise-dev/users';
 import dotenv from 'dotenv';
+// import genreModel from '../api/genres/genreModel'
+import movieModel from '../api/movies/movieModel';
+import movies from '../initialise-dev/movies';
 
 dotenv.config();
 
+// deletes all user documents in collection and inserts test data
 // deletes all user documents in collection and inserts test data
 async function loadUsers() {
   console.log('load user Data');
@@ -17,18 +17,6 @@ async function loadUsers() {
     console.info(`${users.length} users were successfully stored.`);
   } catch (err) {
     console.error(`failed to Load user Data: ${err}`);
-  }
-}
-
-// deletes all user documents in collection and inserts test data
-async function loadGenres() {
-  console.log('load genre Data');
-  try {
-    await genreModel.deleteMany();
-    await genreModel.collection.insertMany(genres);
-    console.info(`${genres.length} genres were successfully stored.`);
-  } catch (err) {
-    console.error(`failed to Load genre Data: ${err}`);
   }
 }
 
@@ -45,8 +33,7 @@ export async function loadMovies() {
   }
 }
 
-if (process.env.SEED_DB == 'true') {
+if (process.env.NODE_ENV === 'development') {
   loadUsers();
-  loadGenres();
   loadMovies();
 }
